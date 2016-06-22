517663c0fbc6b4a2aec1075a1578dea65a416ae3
Cedric Honnet <honnet@telecom-paristech.org>
2016-06-22 19:07:10 +0800

    Now use internal RC oscillator as low power clock when inactive.
         The NRF_CLOCK_LFCLKSRC_RC_250_PPM_TEMP_4000MS_CALIBRATION would be ideal
         but it's not available in this SDK. As this recalibration is useful only
         if the temperature changes and it takes 17ms to do, recalibrating every
         8000MS doesn't seem too risky. More explanations here:
         devzone.nordicsemi.com/question/953/what-low-frequency-clock-sources-can-i-use

--- src/twi_ble_stack.c
+++ src/twi_ble_stack.c
@@ -8,7 +8,13 @@
 
 void ble_stack_init(void) {
   // Initialize the SoftDevice handler module.
-  SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_SYNTH_250_PPM, false);
+  SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_RC_250_PPM_8000MS_CALIBRATION, false);
+  /* The NRF_CLOCK_LFCLKSRC_RC_250_PPM_TEMP_4000MS_CALIBRATION would be ideal
+     but it's not available in this SDK. As this recalibration is useful only
+     if the temperature changes and it takes 17ms to do, recalibrating every
+     8000MS doesn't seem too risky. More explanations here :
+     devzone.nordicsemi.com/question/953/what-low-frequency-clock-sources-can-i-use
+  */
   // Register with the SoftDevice handler module for BLE events.
   ERR_CHECK(softdevice_ble_evt_handler_set(ble_evt_dispatch));
   // Register with the SoftDevice handler module for BLE events.
